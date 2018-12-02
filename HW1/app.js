5e25abb3a5f1136f148df860efb66a2ece00c69a
y.golovko <yuriy.golovko@stoloto.ru>
2018-12-02 19:51:10 +0300

    save

--- HW1/app.js
+++ HW1/app.js
@@ -8,11 +8,10 @@ const fs = require('fs'),
 let rl;
 let args;
 let shouldDelete = false;
-let pathSep = path.sep;
+const pathSep = path.sep;
 
 init = () => {
     args = process.argv.slice(2) || [];
-    console.log('dir', dir);
 
     rl = readline.createInterface({
         input: process.stdin,
@@ -23,9 +22,125 @@ init = () => {
 };
 
 main = () => {
+    //считываем входные данные
+    if(args && args.length <= 3 && args.length >= 2){
+        const [sDir, dDir, flag] = args;
+        shouldDelete = flag === 'y';
+        findAndCopy(sDir, dDir);
+    }else{
+        askFolders();
+    }
+
+    function findAndCopy(srcDir, destDir){
+        //console.log('Копируем из \'', srcDir, '\' в \'', destDir, '\'', shouldDelete ? ('и удаляем \' ' + srcDir + ' \'') : '');
+        fs.readdir(srcDir, (err, files) => {
+            if(err) throw err;
+
+            files.forEach((fileName) => {
+                let filePath = [srcDir, fileName].join(pathSep);
+
+                fs.stat(filePath, (err, fileStat) => {
+                    if(err) throw err;
+
+                    if(fileStat.isDirectory()){
+                        findAndCopy(filePath, destDir);
+                    }else{
+                        let letter = fileName.substr(0, 1).toUpperCase();
+                        let dFolder = [destDir, letter].join(pathSep);
+                        let srcStream = fs.createReadStream(filePath);
+
+                        // srcStream.on('close', () => {
+                        //     console.log('close', srcStream.path);
+                        // });
+                        // srcStream.on('data', () => {
+                        //     console.log('data', srcStream.path);
+                        //     files.push(srcStream.path);
+                        // });
+                        srcStream.on('end', () => {
+                            console.log('end', srcStream.path);
+                        });
+
+                        fs.mkdir(dFolder, {recursive: true}, (err) => {
+                            if(err) throw err;
+
+                            let dFilePath = [dFolder, fileName].join(pathSep);
+                            let destStream = fs.createWriteStream(dFilePath);
+
+                            srcStream.pipe(destStream);
+
+                            destStream.on('finish', () => {
+                                console.log('finish', destStream.path);
+                            });
+                            // destStream.on('close', () => {
+                            //     console.log('close', destStream.path);
+                            // });
+                            // destStream.on('drain', () => {
+                            //     console.log('drain', destStream.path);
+                            // });
+                            // destStream.on('unpipe', () => {
+                            //     console.log('unpipe', destStream.path);
+                            // });
+                            // console.log('copy: ', srcFile.path, '=>', destFile.path);
+                            // console.log('srcFile', srcFile);
+                            // console.log('destFile', destFile);
+                        });
+                    }
+                });
+            });
+        });
+    }
+
+    function askFolders(){
+        rl.question('Введите имена исходной и итоговой папок через пробел: ', answer => {
+            if(answer !== ''){
+                let folders = answer.split(' ');
+
+                if(folders && folders.length === 2){
+                    askFlag(folders);
+                }else{
+                    console.log('Неверное количество папок, повторите ввод. ');
+                    askFolders();
+                }
+            }else{
+                console.log('Папки не заданы, повторите ввод. ');
+                askFolders();
+            }
+        });
+    }
+
+    function askFlag(folders){
+        rl.question('Удалить исходную папку? (y/n): ', answer => {
+            if(answer === 'y' || answer === 'n'){
+                shouldDelete = answer === 'y';
+                findAndCopy(folders[0], folders[1]);
+            }else{
+                askFlag(folders);
+            }
+        });
+    }
+};
+
+/*main = () => {
     if(args && args.length <= 3 && args.length >= 2){
         let flag = !!(args[2] && args[2] === 'y');
-        copy(args, flag);
+        let mainDestFolder = args[1];
+        // проверяем существование директории назначения
+        fs.readdir(mainDestFolder, (err, files) => {
+            if(err){
+                // создаем ее, если она отсутствует и начинаем копирование
+                if(err.code === 'ENOENT') {
+                    fs.mkdir(mainDestFolder, (err) => {
+                        if (err) throw err;
+                        copy(args, flag);
+                    });
+                }else{
+                    throw err;
+                }
+            }else{
+                // или копируем сразу, если директория есть
+                copy(args, flag);
+            }
+        });
     }else{
         askFolders();
     }
@@ -90,26 +205,54 @@ findFiles = (dir, dest) => {
 
 putFile = (filePath, fileName, destPath) => {
     let letter = fileName.substr(0, 1).toUpperCase();
+    let destFolder = [destPath, letter].join(pathSep);
     destPath = [destPath, letter, fileName].join(pathSep);
 
-    console.log(filePath, ' -> ', destPath);
+    //console.log(filePath, ' -> ', destPath);
 
     let srcFile = fs.createReadStream(filePath);
-    let destFile = fs.createWriteStream(destPath);
 
-    srcFile.on('error', (err) => {
-        console.log('src err', err);
-    });
-    srcFile.on('end', (err, a) => {
-        console.log('src end', err, a);
+    //проверяем существование директории dest/буква/
+    fs.readdir(destFolder, (letterFolderErr, files) => {
+        console.log('readdir:', destFolder, files, letterFolderErr ? letterFolderErr.code : '');
+        if(letterFolderErr){
+            if(letterFolderErr.code === 'ENOENT'){
+                fs.mkdir(destFolder, (err) => {
+                    if(err){
+                        if(err.code === 'EEXIST'){
+                            console.log('mkdir', err);
+                        }
+                    }
+                    //save file
+                    let destFile = fs.createWriteStream(destPath);
+                    srcFile.pipe(destFile);
+                });
+            }else{
+                throw letterFolderErr;
+            }
+        }else{
+            //save file
+            let destFile = fs.createWriteStream(destPath);
+            srcFile.pipe(destFile);
+        }
     });
 
-    destFile.on('error', (err) => {
-        console.log('dest err', err);
-    });
-
-    srcFile.pipe(destFile);
-};
+    // let srcFile = fs.createReadStream(filePath);
+    // let destFile = fs.createWriteStream(destPath);
+    //
+    // srcFile.on('error', (err) => {
+    //     console.log('src err', err);
+    // });
+    // srcFile.on('end', (err, a) => {
+    //     console.log('src end', err, a);
+    // });
+    //
+    // destFile.on('error', (err) => {
+    //     console.log('dest err', err);
+    // });
+    //
+    // srcFile.pipe(destFile);
+};*/
 
 init();
 main();
