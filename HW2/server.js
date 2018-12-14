800b39ce864ac0b156aefa85930bc6055acd7a7c
y.golovko <yuriy.golovko@stoloto.ru>
2018-12-14 19:07:18 +0300

    HW2 save

--- HW2/server.js
+++ HW2/server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    console.log('req', req);
+    //console.log('req', req.query);
     //console.log('req', res);
-    res.send('Hello World!');
+    res.send('Привет!<br />' +
+        'Гет запрос инициировал процессы на стороне сервера,<br />' +
+        'через некоторе время здесь будет ответ.<br />' +
+        'Ждите...<br />');
+
+    let tid = setInterval(() => {
+        const currentDate = new Date();
+        console.log(currentDate);
+    }, 1000);
 });
 
 app.listen(3000, function(){
