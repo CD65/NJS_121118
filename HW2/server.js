5f7d564262eb87bacdde79d558f4bd510e4942d1
Lemmy <my3thbox@mail.ru>
2018-12-18 03:05:34 +0300

    home work 2 save2

--- HW2/server.js
+++ HW2/server.js
@@ -1,18 +1,43 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
+const args = process.argv.slice(2);
+console.log('args', args);
+
+const INTERVAL = args[0] || process.env.INTERVAL || 1000;
+const STOP = args[1] || process.env.STOP || 5000;
+
+//console.log('process.env', process.env);
+console.log('INTERVAL', INTERVAL);
+console.log('STOP', STOP);
+
+let currentDate;
+
+process.on('exit', () => console.log('Завершение программы.'));
+
+app.get('/', function(req, res, next){
     //console.log('req', req.query);
     //console.log('req', res);
     res.send('Привет!<br />' +
-        'Гет запрос инициировал процессы на стороне сервера,<br />' +
-        'через некоторе время здесь будет ответ.<br />' +
+        'GET запрос инициировал процессы на стороне сервера,<br />' +
+        'примерно через ' + STOP + ' мс здесь будет ответ.<br />' +
         'Ждите...<br />');
 
-    let tid = setInterval(() => {
-        const currentDate = new Date();
+    let TID = setInterval(() => {
+        currentDate = new Date();
         console.log(currentDate);
-    }, 1000);
+    }, INTERVAL);
+
+    let STOP_TID = setTimeout(() => {
+        clearInterval(TID);
+        console.log('Дата и время на момент остановки таймера (UTC): ', currentDate);
+        next();
+        clearTimeout(STOP_TID);
+        process.exit();
+    }, STOP);
+}, function(){
+    console.log('next fn', this);
+    res.send('Дата и время на момент остановки таймера (UTC): ' + currentDate);
 });
 
 app.listen(3000, function(){
