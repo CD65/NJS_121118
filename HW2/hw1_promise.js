e7181f34c5ea287e273262ed274449882165f461
y.golovko <yuriy.golovko@stoloto.ru>
2018-12-19 22:27:16 +0300

    hw2 save

--- HW2/hw1_promise.js
+++ HW2/hw1_promise.js
@@ -15,24 +15,163 @@ process.on('exit', () => console.log('Завершение программы.')
 main = () => {
     let detectedFiles = [];
     let detectedFolders = [];
+    let destPath = 'dest';
 
     //считываем входные данные
     if(args && args.length <= 3 && args.length >= 2){
         const [sDir, dDir, flag] = args;
         shouldDelete = flag === 'y';
+        destPath = dDir;
 
-        findAndCopy(sDir, dDir);
+        ft(sDir).then(res => {
+            console.log('Файлы скопированы: ', res);
+        });
+        // readDir(sDir, dDir).then(arr => {
+        //     console.log('readDirR res', arr);
+        //     Promise.all(arr).then(res => {
+        //         console.log('promise.all res', res);
+        //         process.exit();
+        //     });
+        // });
     }else{
         askFolders();
     }
 
-    const findAndCopy = async (srcPath, destPath) => {
-        try{
+    function ft(src) {
+        return new Promise(resolve => {
+            let arr = [];
+            function findAndCopy(srcPath){
+                fs.stat(srcPath, (er, file) => {
+                    if(file.isDirectory()){
+                        rd(srcPath).then(res => {
+                            res.forEach(item => {
+                                console.log('item', item);
+                                findAndCopy(item);
+                            });
+                        });
+                    }else{
+                        arr.push(copyFile(srcPath, destPath));
+                    }
+                });
+            }
+            findAndCopy(src);
+            resolve(arr);
+        });
+    }
+    // function findAndCopy(srcPath){
+    //     fs.stat(srcPath, (er, file) => {
+    //         if(file.isDirectory()){
+    //             rd(srcPath).then(res => {
+    //                 res.forEach(item => {
+    //                     console.log('item', item);
+    //                     findAndCopy(item);
+    //                 });
+    //             });
+    //         }else{
+    //             arr.push(copyFile(srcPath, destPath));
+    //         }
+    //     });
+    // }
 
-        }catch(e){
+    function rd(srcPath){
+        return new Promise((resolve) => {
+            fs.readdir(srcPath, (er, files) => {
+                let arr = [];
+                files.forEach(fileName => {
+                    let filePath = [srcPath, fileName].join(sep);
+                    arr.push(filePath);
+                });
+                resolve(arr);
+            });
+        });
+    }
 
-        }
-    };
+    function copyFile(srcFile, destPath){
+        let pathArr = srcFile.split(sep);
+        let fileName = pathArr[pathArr.length - 1];
+        let letter = fileName.substr(0, 1).toUpperCase();
+        let destFolder = [destPath, letter].join(sep);
+        let srcStream = fs.createReadStream(srcFile);
+
+        return new Promise((resolve, reject) => {
+            // создаем директорию
+            fs.mkdir(destFolder, {recursive: true}, er => {
+                if(er) reject(er);
+                // и записываем в нее файл
+                let destFilePath = [destFolder, fileName].join(sep);
+                let destStream = fs.createWriteStream(destFilePath);
+
+                srcStream.pipe(destStream);
+
+                destStream.on('close', () => {
+                    // если нужно удалить файлы из источника
+                    if(shouldDelete){
+                        fs.unlink(srcStream.path, err => {
+                            if(err) throw err;
+                            resolve(srcStream.path);
+                        });
+                    }else{
+                        resolve(srcStream.path);
+                    }
+                });
+            });
+        });
+    }
+
+    function readDir(srcPath, destPath){
+        return new Promise((resolve, reject) => {
+            fs.readdir(srcPath, (er, files) => {
+                let arr = [];
+                files.forEach(fileName => {
+                    let filePath = [srcPath, fileName].join(sep);
+                    arr.push(copyFile(filePath, destPath));
+                });
+                resolve(arr);
+            });
+        });
+    }
+
+    // function rd(srcPath, destPath){
+    //     //return new Promise((resolve, reject) => {
+    //         fs.readdir(srcPath, (er, files) => {
+    //             files.forEach(fileName => {
+    //                 let filePath = [srcPath, fileName].join(sep);
+    //
+    //                 fs.stat(filePath, (er, file) => {
+    //                     if(file.isDirectory()){
+    //
+    //                     }else{
+    //
+    //                     }
+    //                 });
+    //             });
+    //         });
+    //     //});
+    // }
+
+    // const makeRequest = (index = 0) => {
+    //     if (!dataArr[index]) {
+    //         return Promise.resolve();
+    //     }
+    //     return new Promise((resolve, reject) => {
+    //         ajaxRequest(dataArr[index]).then(() => makeRequest(index + 1));
+    //     });
+    // };
+    // makeRequest();
+
+    // function foo(srcPath, destPath) {
+    //     function doo() {
+    //         fs.stat(srcPath, (er, file) => {
+    //             // always return a promise
+    //             if (file.isDirectory()) {
+    //                 return readDir(srcPath, destPath).then(doo);
+    //             } else {
+    //                 return Promise.resolve();
+    //             }
+    //         });
+    //     }
+    //     return doo(); // returns a promise
+    // }
 
     function askFolders(){
         rl.question('Введите имена исходной и итоговой папок через пробел: ', answer => {
